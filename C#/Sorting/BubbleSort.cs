09ef7c272b82a0a4eb6480742f7018c23c6fff89
Nørdic <55633950+nordic16@users.noreply.github.com>
2021-01-10 12:14:42 +0000

    Update BubbleSort.cs

--- C#/Sorting/BubbleSort.cs
+++ C#/Sorting/BubbleSort.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Program {
 	
-	// Used to generate pseudo-random values.
+    // Used to generate pseudo-random values.
     private static readonly Random random = new Random();
 	
     public static void Main(string[] args) {
@@ -17,26 +17,25 @@ public class Program {
 			    
         bubbleSort(arr);
 			
-		Console.WriteLine($"AFTER SORTING:\n[{string.Join(", ", arr)}]");
-        }
+	Console.WriteLine($"AFTER SORTING:\n[{string.Join(", ", arr)}]");
+    }
         
-	// Bubble sort is a very simple and easy to understand sorting algorithm.
-	// It consists of comparing if the next value is out of place, then swaps it with the currrent one.
-	// NOTE: bubble sort is not recommended for very large collections.
+    // Bubble sort is a very simple and easy to understand sorting algorithm.
+    // It consists of comparing if the next value is out of place, then swaps it with the currrent one.
+    // NOTE: bubble sort is not recommended for very large collections.
     private static void bubbleSort(int[] arr) {
             
-		for (int i = 0; i < arr.Length; i++) {
-			for (int j = 0; j < arr.Length - 1; j++) {
+	for (int i = 0; i < arr.Length; i++) {
+	    for (int j = 0; j < arr.Length - 1; j++) {
 					
-				// If the current value is bigger than the next, swap their orders.
-				if (arr[j] > arr[j + 1]) {
-					// Perform interchange
-						
-					int temp = arr[j + 1];
-					arr[j + 1] = arr[j];
-					arr[j] = temp;
-					}
-				}
-			}
-        }
+		// If the current value is bigger than the next, swap their orders.
+		if (arr[j] > arr[j + 1]) {
+		    // Perform interchange			
+			int temp = arr[j + 1];
+			arr[j + 1] = arr[j];
+			arr[j] = temp;
+		}
+	    }    
+	}
     }
+}
