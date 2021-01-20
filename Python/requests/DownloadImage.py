ad21d57b0b68054d3432d4067fc207f3a490fcdd
Nørdic <55633950+nordic16@users.noreply.github.com>
2021-01-20 22:02:34 +0000

    Update DownloadImage.py

--- Python/requests/DownloadImage.py
+++ Python/requests/DownloadImage.py
@@ -1,7 +1,6 @@
 import requests
 
 
-# Make sure to check out the output!
 if __name__ == '__main__':
     req = requests.get('https://ichef.bbci.co.uk/news/976/cpsprodpb/12A9B/production/_111434467_gettyimages-1143489763.jpg')
     
