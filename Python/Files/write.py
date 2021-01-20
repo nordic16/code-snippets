12ddb368f2c3f503cb9d189fccb7baa5b64b490d
Nørdic <55633950+nordic16@users.noreply.github.com>
2021-01-20 22:08:42 +0000

    Create write.py

---
+++ Python/Files/write.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    path = 'path/to/file'
+    
+    # Opens (or creates a file) and writes to it.
+    with open(file, 'w') as file:
+        file.write('I am very cool!')
+    
