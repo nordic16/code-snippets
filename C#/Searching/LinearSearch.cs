30efd4c93f4a0b5a0ca656184cc9cfd5bf63d05f
Nørdic <55633950+nordic16@users.noreply.github.com>
2021-01-24 16:59:59 +0000

    Create LinearSearch.cs

---
+++ C#/Searching/LinearSearch.cs
@@ -0,0 +1,12 @@
+public class LinearSearch {
+    
+    private static readonly Random random = new Random();
+
+    static void main() {
+        int[] arr = new int[10]
+        
+        for (int i = 0; i < arr.Length; i++) {
+            arr[i] = rand.NextInt();
+        }
+    }
+}
