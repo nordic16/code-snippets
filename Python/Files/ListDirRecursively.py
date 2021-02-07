520b4300b63bdb7f45946257418494274e2d3b6c
nordic16 <diogo6204@gmail.com>
2021-02-07 15:00:58 +0000

    Added ListDirRecursively.py

---
+++ Python/Files/ListDirRecursively.py
@@ -0,0 +1,28 @@
+if __name__ == "__main__":
+    import sys, os
+
+
+    """No need for a base case, as when this function takes in an empty
+       folder, the loop won't even execute."""
+    def ListDirRecursively(path: str, indentLevel: int = 0):
+        # For more info on os.walk(), check out 
+        # https://docs.python.org/3/library/os.html#os.walk
+        for root, dirs, files in os.walk(path):
+            for fPath in files:
+                print(os.path.join(root, fPath))
+
+            for dirPath in dirs:
+                ListDirRecursively(dirPath, indentLevel + 4)
+
+
+
+
+    # If path is empty, use the current dir path, otherwise use the specified one.
+    path = sys.argv[1] if len(sys.argv) > 1 else os.path.dirname(os.path.realpath(sys.argv[0]))
+    
+    ListDirRecursively(path=path)
+  
+
+    
+
+   
